a0ba67c2211858b84d67702ffbbd6aff6e01c06c
consciouSoul <146874689+consciouSoul@users.noreply.github.com>
2025-09-19 20:57:35 +0600

    Update config.py

--- config.py
+++ config.py
@@ -6,16 +6,12 @@ load_dotenv()
 MongoURI = os.environ.get("MongoURI")
 
 links = """
-https://vast-superficial-joggers.glitch.me/
-https://vivacious-zenith-parsley.glitch.me/
 https://mdc-7cbt.onrender.com/
 https://report-league.onrender.com/
-https://the-quran-bot-3odr.onrender.com/
-https://quran-2-xtn5.onrender.com/
 
-https://contest-hive.vercel.app/
 https://contest-hive.vercel.app/api/all
 https://toph-api.onrender.com/status
 https://quranapi.pages.dev/api/1.json
 https://nusab19.pages.dev/
 """.split()
+
